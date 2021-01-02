e66089a36495e0ef7decbe24c8b3deef26b369bc
Kihyeon KWON <54575770+kihyeon04@users.noreply.github.com>
2021-01-02 20:18:51 +0900

    init

---
+++ src/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = (props) => {
+  return (
+    <div className="spinner ui segment">
+      <div className="ui active dimmer">
+        <div className="ui text loader">{props.message}</div>
+      </div>
+      <p></p>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    console.log("My comp was rendered");
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+        console.log(err);
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("Updated");
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return (
+      <Spinner message="Please allow location. Your location is saved on your 'browser' in a special place called 'cache'. We will never know your private information!" />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
